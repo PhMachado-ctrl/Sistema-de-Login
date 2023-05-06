5f4541a25132d29e40d916a249cdcfd4af35ce6a
PhMachado-ctrl <phmachado555@gmail.com>
2023-05-06 11:56:44 -0300

    Login

--- sisLogin.py
+++ sisLogin.py
@@ -7,7 +7,7 @@ layout = [
     [sg.Input(key='usuario')],
     [sg.Text('Senha')],
     [sg.Input(key='senha')],
-    [sg.Button('Login')],
+    [sg.Button('login')],
     [sg.Text('',key='mensagem')],
 ]
 
@@ -17,6 +17,16 @@ window = sg.Window('Login',layout=layout)
 '''Loop de Eventos'''
 while True:
     event, values = window.read() #Lê os valores dos eventos que estão na Tela
-    #Quando a tela for fechada pare a execução
-    if event == sg.WIN_CLOSED:
+    
+    if event == sg.WIN_CLOSED: #Quando a tela for fechada pare a execução
         break
+    elif event == 'login': #se acontecer o evento de apertar o botão de login
+        senha_correta = '12345'
+        usuario_correto = 'jonathan'
+        usuario = values['usuario']
+        senha = values['senha']
+
+        if senha == senha_correta and usuario == usuario_correto:
+            window['mensagem'].update('Login feito com Sucesso')
+        else:
+            window['mensagem'].update('Senha ou Usuario incorreto')
